0aa14303ef69c15303d0d314eb228aa7fe45a26b
Kryz Ale <kryzale@gmail.com>
2018-10-23 22:37:14 -0500

    Add

---
+++ AbstractMethodEjemplo/src/abstractfactory/Lista.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstractfactory;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Kryz Ale
+ */
+public abstract class Lista {
+    private ArrayList<String> elementos = new ArrayList();
+    // -------------------------------
+    public String getElemento( int posicion ) {
+        return this.elementos.get( posicion );
+    }
+    // -------------------------------
+    public void insertarElemento( String texto ) {
+        this.elementos.add( texto );
+    }
+    // -------------------------------
+    public void dibujar()
+    {
+        System.out.println("Dibujando la lista...");
+    }
+    // -------------------------------
+    // Método a implementar por las clases que hereden
+     public abstract void configurar();
+}
